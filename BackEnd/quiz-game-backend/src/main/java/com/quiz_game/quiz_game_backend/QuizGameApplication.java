2dcbda15c71c9613c36a290283d488795d99cf63
Lautaro Trabajo <lautaro.fuente@yahoo.com>
2024-09-11 14:00:03 -0300

    init

---
+++ BackEnd/quiz-game-backend/src/main/java/com/quiz_game/quiz_game_backend/QuizGameApplication.java
@@ -0,0 +1,13 @@
+package com.quiz_game.quiz_game_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizGameApplication.class, args);
+	}
+
+}
---
+++ BackEnd/quiz-game-backend/src/test/java/com/quiz_game/quiz_game_backend/QuizGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quiz_game.quiz_game_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
